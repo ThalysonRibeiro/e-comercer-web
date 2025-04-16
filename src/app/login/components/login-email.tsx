5e1f3ead28a68f3aec36e5e788bcdbd31f334334
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-15 23:38:24 -0300

    feat: page login

--- src/app/login/components/login-email.tsx
+++ src/app/login/components/login-email.tsx
@@ -6,37 +6,39 @@ import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  email: z.string().min(5, 'O email ou celular é obrigatório'),
+  password: z.string().min(8, 'A senha deve ter pelo menos 8 caracteres'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+interface SiginInProps {
+  email: string;
+  password: string;
+}
 
 export function LoginEmail() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  });
 
-    // Validação do lado do cliente
-    if (!email || !password) {
-      setError("Email e senha são obrigatórios");
-      return;
-    }
-
-    setLoading(true);
-    setError("");
-
+  async function onSubmit(data: SiginInProps) {
     try {
-      console.log("Enviando credenciais:", { email, password }); // Log para debug
-
+      setLoading(true);
       const result = await signIn("credentials", {
-        email,
-        password,
+        email: data.email,
+        password: data.password,
         redirect: false,
       });
-
-      console.log("Resultado do login:", result); // Log para debug
-
       if (result?.error) {
         setError("Email ou senha inválidos");
       } else {
@@ -54,31 +56,29 @@ export function LoginEmail() {
   return (
     <div className="border border-gray-400 rounded-lg p-18 space-y-4">
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex max-w-125 w-full gap-6 flex-col items-center justify-center text-center"
       >
         <h1 className="text-2xl font-bold mb-8">Faça login para continuar</h1>
 
-        {error && <p className="text-red-500 text-sm">{error}</p>}
-
         <Input
           type="text"
           placeholder="Email ou Celular"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+          {...register('email')}
         />
+        {errors.email && <p className="text-danger">{errors.email.message}</p>}
 
         <InputPassword
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
           placeholder="Senha"
+          {...register('password')}
         />
+        {errors.email && <p className="text-danger">{errors.password?.message}</p>}
 
         <Button type="submit" disabled={loading}>
           {loading ? "Entrando..." : "Entrar"}
         </Button>
+
+        {error && <p className="text-danger">{error}</p>}
 
         <div className="w-full flex items-center gap-3">
           <hr className="border border-gray-300 w-full" />
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,6 +6,14 @@ import { Input, InputPassword } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+// const passwordSchema = z
+//   .string()
+//   .min(8, "A senha deve ter no mínimo 8 caracteres")
+//   .regex(/[A-Z]/, "A senha deve conter pelo menos uma letra maiúscula")
+//   .regex(/[a-z]/, "A senha deve conter pelo menos uma letra minúscula")
+//   .regex(/[0-9]/, "A senha deve conter pelo menos um número")
+//   .regex(/[\W_]/, "A senha deve conter pelo menos um caractere especial");
+
 export default async function Register() {
   const session = await getServerSession(authOptions);
 
