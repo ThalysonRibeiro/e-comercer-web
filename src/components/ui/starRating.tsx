dbeb36fa6c99c4952f5440c38da243cf319c709b
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-24 09:46:59 -0300

    feat: add propertie size

--- src/components/ui/starRating.tsx
+++ src/components/ui/starRating.tsx
@@ -1,12 +1,13 @@
 import { StarIcon } from "lucide-react";
 import { IoIosStar } from "react-icons/io";
 
-export function StarRating({ rating }: { rating: number }) {
+export function StarRating({ rating, size }: { rating: number, size?: number }) {
   const numericRating = Number(rating / 10);
   return (
     <div className="flex">
       {[...Array(5)].map((_, index) => (
         <IoIosStar
+          size={size}
           key={index}
           className={index < numericRating ? "text-star" : "text-text-secundary"}
         />
