3e208daeb4ff097c9f925f2aacccf3752f5b2cb8
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-24 12:09:35 -0300

    style: format style date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,5 @@
 import { startOfWeek, format } from 'date-fns';
 import { ptBR } from "date-fns/locale";
 export function formatDate(date: Date) {
-  return format(startOfWeek(date), 'd MMM YYY', { locale: ptBR })
+  return format(startOfWeek(date), 'd MMM yyy', { locale: ptBR })
 }
