8fae01a4ea5308b2e6496a9cc847c83ba15358c7
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-27 19:03:57 -0300

    style: buttons

--- src/components/ui/cardProduct.tsx
+++ src/components/ui/cardProduct.tsx
@@ -8,13 +8,17 @@ import { useAppContext } from "@/context/AppContext";
 import Link from "next/link";
 import { formatCurrency } from "@/utils/formatCurrency";
 import no_image from "@/assets/no-image.png";
-import { Heart } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
+import WishButton from "./wishButton";
 
 
 
 export function CardProduct({ product }: { product: ProductsProps }) {
-  const { addItemCart } = useAppContext();
+  const { addItemCart, userData, addItemWishlist } = useAppContext();
 
+  function handleItemWishList(productId: string) {
+    addItemWishlist(productId)
+  }
 
   function handleAddCartItem(product: ProductsProps) {
     addItemCart(product);
@@ -22,7 +26,7 @@ export function CardProduct({ product }: { product: ProductsProps }) {
 
   return (
     <>
-      <div key={product.id} className="relative overflow-hidden flex flex-col justify-between border border-borderColor w-75 h-125 bg-bgCard rounded-lg p-4">
+      <div key={product.id} className="relative overflow-hidden flex flex-col justify-between border border-borderColor w-full h-105 bg-bgCard rounded-lg p-4">
         <Link href={`/products/${product.id}`}>
           {product.bigsale && (
             <div className="bg-primaryColor text-title font-semibold w-35 text-center absolute rotate-45 -right-8 top-7 z-5">Big sale</div>
@@ -30,14 +34,12 @@ export function CardProduct({ product }: { product: ProductsProps }) {
           <Flex className="my-2">
             <StarRating rating={product.rating} />
             <p className="text-xs ml-1">({product.products_sold})</p>
-            <div className="absolute z-10 top-70 left-6">
-              <Heart />
-            </div>
+
             {product.stock === 1 && (
               <p className="font-bold capitalize w-full text-center text-title bg-primaryColor absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">ultimo disponível</p>
             )}
           </Flex>
-          <div className='relative w-65 h-65'>
+          <div className='relative min-w-45 min-h-45'>
             <Image
               src={product.images && product.images.length > 0 ? product.images[0].image : no_image}
               alt={product.title}
@@ -61,9 +63,19 @@ export function CardProduct({ product }: { product: ProductsProps }) {
             <p className="text-oldPrice text-sm">ou até <span className="font-semibold">10x de R$ {(Number((product.price / 100)) / 10).toFixed(2)}</span></p>
           </div>
         </Link>
-        <Button onClick={() => handleAddCartItem(product)}>
-          Add To Cart
-        </Button>
+        <Flex className='justify-between gap-2.5'>
+          <Button onClick={() => handleAddCartItem(product)} className='gap-3 border border-borderColor'>
+            <span className="text-sm">Adicionar</span>
+            <ShoppingCart className="size-6" />
+          </Button>
+          {userData && (
+            <Button
+              onClick={() => handleItemWishList(product.id)}
+              className="w-10 border border-borderColor bg-primaryColor">
+              <WishButton animate={!!product.isLiked} liked={!!product.isLiked} />
+            </Button>
+          )}
+        </Flex>
       </div>
     </>
   )
