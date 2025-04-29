cb4b12d865206e430ae21bd0da873684b896c43d
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-04-29 12:59:31 -0300

    feat:add apikey

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const serverApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, // Nota: sem NEXT_PUBLIC_
+  headers: {
+    'x-api-key': process.env.NEXT_PUBLIC_BACKEND_API_KEY, // substitua pela chave correta
+  },
 });
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,6 +5,9 @@ import { getSession } from "next-auth/react";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  headers: {
+    'x-api-key': process.env.NEXT_PUBLIC_BACKEND_API_KEY, // substitua pela chave correta
+  },
 });
 
 // Interceptor para adicionar token de autenticação
