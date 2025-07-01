ac5d830c1c804b7ce4d8d1caa299ae8642a35bcb
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-01 17:10:07 -0300

    style:adjusted width card

--- src/components/cardProductCarousel.tsx
+++ src/components/cardProductCarousel.tsx
@@ -55,7 +55,7 @@ export function CardProductCarousel({ products }: CardProductsProps) {
 
 
   return (
-    <Flex className='max-w-[1280] w-full items-center mt-6'>
+    <Flex className='w-full items-center mt-6'>
       <Swiper
         spaceBetween={between ? between : 30}
         slidesPerView={responsive ? responsive : 4}
@@ -70,53 +70,54 @@ export function CardProductCarousel({ products }: CardProductsProps) {
       >
         {products?.map(product => (
           <SwiperSlide key={product.id}>
-            <div key={product.id} className="relative overflow-hidden flex flex-col justify-between border border-borderColor min-w-50 h-125 bg-bgCard rounded-lg p-4">
-              <Link href={`/products/${product.id}`}>
+            <div key={product.id} className="relative overflow-hidden flex flex-col justify-between border border-border min-w-50 md:h-125 bg-card rounded-lg p-4">
+              <Link href={`/products/${product.id}`} className='flex flex-row md:flex-col gap-2'>
                 {product.bigsale && (
-                  <div className="bg-primaryColor text-title font-semibold w-35 text-center absolute rotate-45 -right-8 top-7 z-10">Big sale</div>
+                  <div className="bg-primary font-semibold w-35 text-center absolute rotate-45 -right-8 top-7 z-10">Big sale</div>
                 )}
-                <Flex className="my-2">
-                  <StarRating rating={product.rating} />
-                  <p className="text-xs ml-1">({product.products_sold})</p>
-
-                  {product.stock === 1 && (
-                    <p className="font-bold capitalize w-full text-center text-title bg-primaryColor absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">ultimo disponível</p>
-                  )}
-                </Flex>
-                <div className='relative w-full min-h-65'>
+                {product.stock === 1 && (
+                  <p className="font-bold capitalize w-full text-center bg-primary absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">ultimo disponível</p>
+                )}
+                <div className='relative w-80 md:w-full h-30 md:min-h-65 border rounded-lg'>
                   <Image
                     src={product.images && product.images.length > 0 ? product.images[0].image : no_image}
                     alt={product.title}
                     fill
                     sizes="(max-width: 768px) 100vw, 33vw"
-                    className="object-cover rounded-lg"
+                    className="object-contain rounded-lg"
                   />
                 </div>
-                <h1 className="text-title font-semibold text-sm line-clamp-2 mb-2">
-                  {product.title}
-                </h1>
-                <Flex className="items-center justify-between">
-                  <span className="text-xl text-price font-semibold">
-                    {formatCurrency(product.price / 100)}
-                  </span>
-                  <span className="text-sm line-through text-oldPrice">
-                    {formatCurrency(product.old_price / 100)}
-                  </span>
-                </Flex>
-                <div className="mb-2">
-                  <p className="text-oldPrice text-sm">À vista no PIX</p>
-                  <p className="text-oldPrice text-sm">ou até <span className="font-semibold">10x de R$ {(Number((product.price / 100)) / 10).toFixed(2)}</span></p>
+                <div>
+                  <Flex className="my-2">
+                    <StarRating rating={product.rating} />
+                    <p className="text-xs ml-1">({product.products_sold})</p>
+                  </Flex>
+                  <h2 className="font-semibold text-sm line-clamp-2 mb-2">
+                    {product.title}
+                  </h2>
+                  <Flex className="items-center justify-between">
+                    <span className="text-xl text-price font-semibold">
+                      {formatCurrency(product.price / 100)}
+                    </span>
+                    <span className="text-sm line-through text-oldPrice">
+                      {formatCurrency(product.old_price / 100)}
+                    </span>
+                  </Flex>
+                  <div className="mb-2">
+                    <p className="text-oldPrice text-sm">À vista no PIX</p>
+                    <p className="text-oldPrice text-sm">ou até <span className="font-semibold">10x de R$ {(Number((product.price / 100)) / 10).toFixed(2)}</span></p>
+                  </div>
                 </div>
               </Link>
               <Flex className='justify-center gap-2.5'>
-                <Button onClick={() => handleAddCartItem(product)} className='border border-borderColor'>
+                <Button variant='outline' onClick={() => handleAddCartItem(product)} className='border border-border'>
                   <span className="text-sm">Adicionar</span>
                   <ShoppingCart className="size-6" />
                 </Button>
                 {userData && (
                   <Button
                     onClick={() => handleItemWishList(product.id)}
-                    className="w-10 border border-borderColor bg-primaryColor">
+                    className="w-10 border border-border bg-primary">
                     <WishButton animate={!!product.isLiked} liked={!!product.isLiked} />
                   </Button>
                 )}
