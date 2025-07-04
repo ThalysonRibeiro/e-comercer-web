5a6aed033608ae77068ef0a20aaa9f557b3687c4
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-07-04 15:49:59 -0300

    refactor function

--- src/utils/formatCNPJ.ts
+++ src/utils/formatCNPJ.ts
@@ -1,7 +1,18 @@
 export function formatCNPJ(cnpj: string) {
-  return cnpj
-    .replace(/(\d{2})(\d)/, "$1.$2")
-    .replace(/(\d{3})(\d)/, "$1.$2")
-    .replace(/(\d{3})(\d)/, "$1/$2")
-    .replace(/(\d{4})(\d)/, "$1-$2")
+  // Remove caracteres não numéricos
+  const cleanCnpj = cnpj.replace(/\D/g, '');
+
+  // Verifica se tem 14 dígitos
+  if (cleanCnpj.length !== 14) {
+    throw new Error('CNPJ deve ter exatamente 14 dígitos');
+  }
+
+  return cleanCnpj
+    .replace(/(\d{2})(\d)/, "$1.$2")        // XX.
+    .replace(/(\d{2}\.\d{3})(\d)/, "$1.$2") // XX.XXX.
+    .replace(/(\d{2}\.\d{3}\.\d{3})(\d)/, "$1/$2") // XX.XXX.XXX/
+    .replace(/(\d{2}\.\d{3}\.\d{3}\/\d{4})(\d)/, "$1-$2") // XX.XXX.XXX/XXXX-XX
 }
+
+// Exemplo de uso:
+// formatCNPJ("12345678000195") // retorna "12.345.678/0001-95"
